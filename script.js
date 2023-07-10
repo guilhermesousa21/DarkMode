2df89b897eb336bf7d4764806c9a0b049ce1bc42
guilhermesousa21 <guilhermesousaa21@gmail.com>
2023-07-10 11:34:13 -0300

    1

---
+++ script.js
@@ -0,0 +1,7 @@
+const header = document.querySelector('.header') //chamando do html
+const icon = document.querySelector('.icon') //chamando do html
+
+icon.addEventListener('click', () => { //quando clicar no icone
+    header.classList.toggle('dark') //faça isso... css
+}
+)
